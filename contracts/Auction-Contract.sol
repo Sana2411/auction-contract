732a7c957bf5e474a5471ba4655e3a10cf747066
Sana2411 <sanarp7398@gmail.com>
2023-02-23 19:52:38 +0530

    contract completed

--- contracts/Auction-Contract.sol
+++ contracts/Auction-Contract.sol
@@ -7,19 +7,23 @@ contract Auction {
     mapping(string => uint256) public auctionStartTime;
     mapping(string => uint256) public auctionEndTime;
     mapping(address => string[]) private auctionOf;
+    uint256 public highestBid;
+    address public highestBidder;
+    bool public isAuctionEnd;
 
     constructor() {
         Auctioneer = msg.sender;
     }
 
+    event bidInfo(address bidder, uint256 amount, string actionFor);
+
     function getAuctions(address user) public view returns (string[] memory) {
         return auctionOf[user];
     }
 
-    function initialize(
-        string memory AuctionName,
-        uint256 _deadline
-    ) public {
+    function createAuction(string memory AuctionName, uint256 _deadline)
+        public
+    {
         require(
             msg.sender == Auctioneer,
             "Only the auctioneer can create an auction "
@@ -28,5 +32,34 @@ contract Auction {
         auctionStartTime[AuctionName] = block.timestamp;
         auctionEndTime[AuctionName] = _deadline;
         auctionOf[msg.sender].push(AuctionName);
+    }
+
+    function bidding(string memory auctionName) public payable {
+        require(auctionStartTime[auctionName] > 0, "Not a valid Auction");
+        require(
+            auctionStartTime[auctionName] < block.timestamp &&
+                auctionEndTime[auctionName] > block.timestamp,
+            "Time out"
+        );
+        require(msg.value > highestBid, "Please bid higher amount");
+        require(Auctioneer != msg.sender, "caller is not the owner");
+        if (highestBid != 0) {
+            bidAmount[msg.sender] += highestBid;
+        }
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+
+        emit bidInfo(msg.sender, msg.value, auctionName);
+    }
+
+    function endAuction(string memory auctionName) public {
+        require(msg.sender == Auctioneer, "caller is not the owner");
+        require(
+            auctionEndTime[auctionName] < block.timestamp,
+            "Auction not yet ended"
+        );
+        require(!isAuctionEnd, "Auction has already ended");
+        isAuctionEnd = true;
+        payable(Auctioneer).transfer(highestBid);
     }
 }
